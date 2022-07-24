6ca8e8c844b28e987c37e55595e9307ec6b5c69f
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-07-24 11:08:38 -0300

    feat: adicionando mais dados para conexao com o banco

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
-// Update with your config settings.
-
 require('dotenv').config();
 
 module.exports = {
     client: 'mysql',
     connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       database: process.env.DB_NAME,
       user:     process.env.DB_USER,
       password: process.env.DB_PASSWORD
