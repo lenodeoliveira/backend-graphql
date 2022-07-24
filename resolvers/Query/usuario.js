bd0aad3969955fa3cebc5049f27b9964e28d94dc
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-07-24 15:41:10 -0300

    feat: adiciona funcao para login

--- resolvers/Query/usuario.js
+++ resolvers/Query/usuario.js
@@ -1,6 +1,18 @@
 const db = require('../../config/db')
+const bcrypt = require('bcrypt-nodejs')
+const  { getUsuarioLogado } = require('../comum/usuario')
 
 module.exports = {
+    async login(_, { dados }) {
+        const usuario = await db('usuarios')
+                                .where({ email: dados.email })
+                                .first()
+        if(!usuario) {
+            throw new Error('Usuário/Senha inválido')
+        }
+
+        const saoIguais = bcrypt.compareSync(dados.senha, )
+    },
     usuarios() {
         return db('usuarios')
     },
